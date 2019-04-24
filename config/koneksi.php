fa3b0a329a922bb450676fb59fe6023042624504
Ahmad Hidayat <comp.ahmadhidayat@gmail.com>
2019-04-25 02:51:38 +0700

    asd

--- config/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server		= "localhost:3306";
-$username	= "root";
-$password	= "";
-$db			= "blaur_vact";
-
-$connect	= new mysqli($server,$username,$password,$db) or die (mysql_error());
-?>
--- logout_admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-  session_start();
-  session_destroy();
-  header('location:../GoTicket_user/Project/login.php');
-?>
