41f0a18378adfa4cfc0c74b8a2cf2689b365db31
Ahmad Hidayat <comp.ahmadhidayat@gmail.com>
2019-04-25 00:28:31 +0700

    ad

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html class="no-js">
     
     <head>
-        <title>Admin Home PAGE</title>
+        <title>Admin Home Pagess</title>
         <!-- Bootstrap -->
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
